e2352263142609c5670c8f45e85b034cc8f61d19
Shridhar-dev <shridharkamat10@gmail.com>
2021-11-06 17:58:14 +0530

    added missing alt tags and lang attr to html

--- components/CSSGallery.js
+++ components/CSSGallery.js
@@ -27,22 +27,22 @@ function CSSGallery() {
                 <CSSArt src={Discord.src}  width={500} alt="Img of Discord Logo made in Pure CSS"/>
                 <CSSArt src={Android.src}  width={200} alt="Img of Android made in Pure CSS"/>
                 <React.Fragment className="d-block d-md-none">
-                    <CSSArt src={AarogyaSetu.src} width={300}/>
-                    <CSSArt src={ReactLogo.src}  width={300}/>
-                    <CSSArt src={Discord.src}  width={500} />
-                    <CSSArt src={Android.src}  width={200}/>
-                    <CSSArt src={AarogyaSetu.src} width={300}/>
-                    <CSSArt src={ReactLogo.src}  width={300}/>
+                    <CSSArt src={AarogyaSetu.src} width={300} alt="Img of Aarogya Setu Logo made in Pure CSS"/>
+                    <CSSArt src={ReactLogo.src}  width={300} alt="Img of React Logo made in Pure CSS"/>
+                    <CSSArt src={Discord.src}  width={500} alt="Img of Discord Logo made in Pure CSS"/>
+                    <CSSArt src={Android.src}  width={200} alt="Img of Android Logo made in Pure CSS"/>
+                    <CSSArt src={AarogyaSetu.src} width={300} alt="Img of Aarogya Setu Logo made in Pure CSS"/>
+                    <CSSArt src={ReactLogo.src}  width={300} alt="Img of React Logo made in Pure CSS"/>
                 </React.Fragment>
             </div>
             <small className="d-block d-md-none pb-5 mb-2 text-left w-100" style={{color:'white',fontSize:'1rem',opacity:'0.7'}}>Scroll ➡️</small>
             <div className="d-none d-md-flex mx-5 mx-md-0 my-5 py-2 justify-content-center align-items-center css-scroll" data-scroll data-scroll-speed="-5" data-scroll-repeat data-scroll-direction="horizontal">
-                <CSSArt src={AarogyaSetu.src} width={300}/>
-                <CSSArt src={ReactLogo.src}  width={300}/>
-                <CSSArt src={Discord.src}  width={500} />
-                <CSSArt src={Android.src}  width={200}/>
-                <CSSArt src={AarogyaSetu.src} width={300}/>
-                <CSSArt src={ReactLogo.src}  width={300}/>
+                <CSSArt src={AarogyaSetu.src} width={300} alt="Img of Aarogya Setu Logo made in Pure CSS"/>
+                <CSSArt src={ReactLogo.src}  width={300} alt="Img of React Logo made in Pure CSS"/>
+                <CSSArt src={Discord.src}  width={500} alt="Img of Discord Logo made in Pure CSS"/>
+                <CSSArt src={Android.src}  width={200} alt="Img of Android Logo made in Pure CSS"/>
+                <CSSArt src={AarogyaSetu.src} width={300} alt="Img of Aarogya Setu Logo made in Pure CSS"/>
+                <CSSArt src={ReactLogo.src}  width={300} alt="Img of React Logo made in Pure CSS"/>
             </div>
             <a href="https://codepen.io/shridhar-dev">
                 <small style={{color:'white',fontSize:'1.3rem',opacity:'0.7'}}><u>Check all of them on codepen</u></small>
--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 }
