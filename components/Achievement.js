e3af0642e470ff5616ec72708c937793d68c23f2
Shridhar-dev <shridharkamat10@gmail.com>
2021-11-06 18:23:32 +0530

    added missing alt tags and robots.txt

--- components/Achievement.js
+++ components/Achievement.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Achievement(props) {
     return (
         <div className="w-100 text-white d-block d-md-flex px-5 py-5 mt-5" style={{background:'black'}}>
-            <img src={props.svg} className="img-fluid mb-5 mb-md-0" width="100"/>
+            <img src={props.svg} className="img-fluid mb-5 mb-md-0" width="100" alt={props.alt}/>
             <div className="w-100 d-flex justify-content-start flex-column align-items-start px-4">
                 <h2 className="text-start">{props.heading}</h2>
                 <h6 className="mt-3 text-start" style={{lineHeight:'1.4rem',color:'rgba(255,255,255,0.7)'}}>{props.text}</h6>
--- components/Achievements.js
+++ components/Achievements.js
@@ -6,8 +6,8 @@ function Achievements() {
         
         <div className="container text-center  py-5 mt-5" style={{position:'relative',margin:'0% 80%'}}>
             <h1 className="display-5 text-center mb-5 pb-2" style={{fontWeight:'700',color:'white'}}>Achievements</h1>
-            <Achievement svg={Trophy.src} heading={"Google Code to Learn 2018 Finalist"} text={"Was Finalist in Google Code to Learn 2018 competition. Made a game in Scratch that revolved around a detective trying to solve a mystery"}/>
-            <Achievement svg={Trophy.src} heading={"Hackathon Winner"} text={"Best Beginner Hack Winner at Hack At Home II Hackathon organized by MLH (Major League Hacking)"}/>
+            <Achievement svg={Trophy.src} alt={"trophy"} heading={"Google Code to Learn 2018 Finalist"} text={"Was Finalist in Google Code to Learn 2018 competition. Made a game in Scratch that revolved around a detective trying to solve a mystery"}/>
+            <Achievement svg={Trophy.src} alt={"trophy"} heading={"Hackathon Winner"} text={"Best Beginner Hack Winner at Hack At Home II Hackathon organized by MLH (Major League Hacking)"}/>
         </div>
     )
 }
