7ee3725b2ed3fdb11c89d57e859b6c2591979717
Shridhar-dev <shridharkamat10@gmail.com>
2021-11-27 15:07:26 +0530

    locomotive content load error fixed

--- pages/_app.js
+++ pages/_app.js
@@ -28,7 +28,7 @@ function MyApp({ Component, pageProps }) {
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700;800&display=swap" rel="stylesheet"/> 
         <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="shridharkamat" data-description="Support me on Buy me a coffee!" data-message="" data-color="#1DA1F2" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       </Head>
-      <main style={{scrollBehavior:'smooth'}}>
+      <main >
         <Component {...pageProps} />
       </main>
     </>
